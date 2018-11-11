393232cd41671f3bd6f6e3f6f93e7191093d9ae7
Imran Chowdhury <imran@imranc.io>
2018-11-12 00:17:48 +1100

    Updated vbox guest script

--- setup/arch_vbox_guest.sh
+++ setup/arch_vbox_guest.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+kernel=$(uname -r | sed -r 's/^([0-9]+)\.([0-9]+).*$/\1\2/')
 # for auto-resize, shared-clipboard, drag-and-drop, etc
-sudo pacman -Sy --needed virtualbox-guest-utils
+# guest modules for arch kernel:
+pacman -Ss virtualbox-guest-modules-arch &&
+sudo pacman -Sy --needed virtualbox-guest-modules-arch
+# guest modules for manjaro kernel:
+pacman -Ss linux$kernel-virtualbox-guest-modules
+sudo pacman -Sy --needed linux$kernel-virtualbox-guest-modules
 # for auto mounting shared folders to /media
 groups | grep -qF "vboxsf" || sudo groupadd vboxsf
 sudo usermod -G vboxsf -a $USER
