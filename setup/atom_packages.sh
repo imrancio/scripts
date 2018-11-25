562130330a4c9d980ef77dd7faef4de19e9ab670
Imran Chowdhury <imran@imrancio.com>
2018-11-25 13:35:06 +1100

    Added Atom packages

---
+++ setup/atom_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Packages for Atom editor
+apm install \
+atom-beautify \
+atom-live-server \
+autoclose-html \
+emmet \
+file-icons \
+highlight-selected \
+minimap \
+minimap-git-diff \
+minimap-highlight-selected \
+minimap-pigments \
+pigments \
+platformio-ide-terminal
